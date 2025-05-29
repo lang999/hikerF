fa4fdb883fd1a35780173baa3573a85a9f908262
lang999 <39425761+lang999@users.noreply.github.com>
2025-05-29 18:56:27 +0800

    Update 茄子.js

--- 茄子.js
+++ 茄子.js
@@ -91,7 +91,7 @@ const csdown = {
                                 }, {
                                     title: '更换线路',
                                     js: $.toString(() => {
-                                        var url = 'http://randomapi02.changfapiaopiao.top|http://randomapi01.changfapiaopiao.top|http://api018.apijiekou.top/api|http://api018.phpjiekou.top|http://api.22s.lol/api|http://api.changfapiaopiao.top|http://api1.apijiekou.top/api|http://api.phpjiekou.top'.split('|');
+                                        var url = 'http://api1.feylen6.top|http://randomapi01.changfapiaopiao.top|http://api018.apijiekou.top/api|http://api018.phpjiekou.top|http://api.22s.lol/api|http://randomapi03.changfapiaopiao.top|http://api1.apijiekou.top/api|http://api.phpjiekou.top'.split('|');
                                         var option = '线路1&线路2&线路3&线路4&线路5&线路6&线路7&线路8'.split('&')
                                         var Line = {
                                             title: '切换线路',
@@ -379,7 +379,7 @@ const csdown = {
         } catch (e) {
             log(e.message)
             if (getMyVar('a') == '') {
-                const host = 'http://randomapi02.changfapiaopiao.top';
+                const host = 'http://randomapi01.changfapiaopiao.top';
                 const shouye = qzDecrypt(request('http://007.22s.lol/encrypt/api.php?path=qiezi/shouye'))
                 const data = qzDecrypt(request('http://007.22s.lol/encrypt/api.php?path=qiezi/zonghe'))
                 const search = fetch('http://007.22s.lol/searchconfig/vipapi/vipconfig.txt')
